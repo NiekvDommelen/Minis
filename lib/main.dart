1e4015f274718e86b5c9a5cdc9fd33ddb107a075
SneakyNieky <niekdommelen37@gmail.com>
2023-10-26 14:54:02 +0200

    Changed in-app title to minis

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Minis',
       theme: ThemeData(
         textTheme: const TextTheme(
           displayLarge: TextStyle(
